e378b3f9fc482b6bd3fc8c617a6088b45a400027
Patrick Henderson <pah17@imperial.ac.uk>
2020-08-17 15:51:15 +0100

    Implementing use of reference document

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# pandocAPI
+# Pandoc API
+**A Haskell web API providing Pandoc as a web service.**
+
+In it's current state, the app allows for a *markdown file* to be converted into either a *pdf* or *docx* file via Pandoc. 
+
+This doesn't provide a wrapper around the Pandoc command line program, but instead, utilises the internal Pandoc functionality available in Haskell.
+
+
+
--- src/Convert.hs
+++ src/Convert.hs
@@ -3,9 +3,11 @@ module Convert (
 ) where
 
 import Text.Pandoc.App (convertWithOpts, defaultOpts, Opt(..))
+import Text.Pandoc.Options (WrapOption (WrapPreserve))
 
 convert :: FilePath -> FilePath -> IO ()
-convert inputFile outputFile = convertWithOpts $ defaultOpts { optInputFiles = Just [inputFile]
-                                                             , optOutputFile = Just outputFile
+convert inputFile outputFile = convertWithOpts $ defaultOpts { optInputFiles   = Just [inputFile]
+                                                             , optOutputFile   = Just outputFile
+                                                             , optReferenceDoc = Just "Ref.docx"
+                                                             , optWrap         = WrapPreserve
                                                              }
-                                                               
